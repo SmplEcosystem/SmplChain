5fe2730ce621d68d89aa9740623b0a1112bbea1e
Jacob Boswell <jacob@s56.net>
2023-01-01 12:16:10 -0700

    added genesis file. removed client

--- ts-client/smplchain.roles/types.ts
+++
@@ -1,7 +0,0 @@
-import { Params } from "./types/smplchain/roles/params"
-
-
-export {     
-    Params,
-    
- }
--- ts-client/smplchain.smplchain/types.ts
+++
@@ -1,7 +0,0 @@
-import { Params } from "./types/smplchain/smplchain/params"
-
-
-export {     
-    Params,
-    
- }
