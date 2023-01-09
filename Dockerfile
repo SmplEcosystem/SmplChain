792a15ebf85e2e0529f53989b6436ecf7fc3b3ab
Jacob Boswell <jacob@s56.net>
2023-01-09 15:39:08 -0700

    got docker-compose to build. Updated script for import

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM golang:1.18-bullseye as build
+
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y build-essential ca-certificates curl
+
+RUN curl https://get.ignite.com/cli! | bash
+
+WORKDIR /app
+
+COPY ./ ./
+
+RUN ignite chain build -o /tmp/go/bin
+RUN ignite chain init
+
+FROM golang:1.18-bullseye
+
+COPY --from=build /go/bin/* /go/bin/
+COPY --from=build /root/.SmplChain /root/.SmplChain
+
+CMD [ "/go/bin/SmplChaind", "start" ]
--- scripts/balance.js
+++ scripts/balance.js
@@ -1,8 +1,26 @@
 
-// copy latest  genesis.json 
-let genesis = require("./genesis.json")
+// copy latest  genesis.json
+
+let file = 'genesis.json'
+if (process.argv[2]) file = process.argv[2];
+
+let genesis = require(`${file}`)
 
 genesis.app_state.bank.balances.forEach(account => {
-    console.log("address",account.address)
-    console.log("amount",account.coins[0].amount)
+    let output = `${account.address} `;
+    account.coins.forEach(coin => {
+        let amount = coin.amount;
+        let denom = coin.denom;
+        if (denom === 'smpl') {
+            amount += "000000000"
+            denom = 'garden'
+        } else if (denom === 'usdse') {
+            amount += "000000"
+            denom = 'busdse'
+        }
+        output += `${amount}${denom}`;
+        console.log(output)
+    })
+    // console.log("address",account.address)
+    // console.log("amount",account.coins[0].amount)
 });
