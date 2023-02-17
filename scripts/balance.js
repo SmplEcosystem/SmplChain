0b235dead69e1161e2bfb3a9f71eee61da3329ca
Jacob Boswell <jacob@s56.net>
2023-02-17 15:23:11 -0700

    added scripts for migration

--- scripts/balance.js
+++ scripts/balance.js
@@ -5,22 +5,33 @@ let file = 'genesis.json'
 if (process.argv[2]) file = process.argv[2];
 
 let genesis = require(`${file}`)
+let smplTotal = 0;
 
 genesis.app_state.bank.balances.forEach(account => {
-    let output = `${account.address} `;
+    let output = `${account.address}`;
+    let authAccount = genesis.app_state.auth.accounts.find(a => a.address === account.address);
+    if(!authAccount || authAccount['@type'] !== '/cosmos.auth.v1beta1.BaseAccount') return;
+    const coins = [];
     account.coins.forEach(coin => {
         let amount = coin.amount;
         let denom = coin.denom;
+
         if (denom === 'smpl') {
+            smplTotal += parseInt(amount);
             amount += "000000000"
             denom = 'garden'
         } else if (denom === 'usdse') {
             amount += "000000"
             denom = 'busdse'
+        } else {
+            console.log('OTHER DENOM')
         }
-        output += `${amount}${denom}`;
-        console.log(output)
+
+        coins.push(`${amount}${denom}`);
     })
+    console.log(`${output} ${coins.join(',')}`)
     // console.log("address",account.address)
     // console.log("amount",account.coins[0].amount)
 });
+
+console.log('total smpl', smplTotal)
---
+++ scripts/genesis-deposites.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+if [ -d "$HOME/.SmplChain" ]; then
+  echo "$HOME/.SmplChain exists, exiting"
+  exit 1
+fi
+
+#if [ "" == "$1" ]; then
+#  echo "Please pass a node moniker"
+#  exit 1
+#fi
+
+file=smpl-chaind-state.txt
+
+denomMetadata=$(<denom-metadata.json)
+
+SmplChaind init "test"
+
+chainName="cosmos:smpl-chain-testing"
+
+genesisFile="$HOME/.SmplChain/config/genesis.json"
+sed -i '/\[api\]/,+3 s/enable = false/enable = true/' "$HOME"/.SmplChain/config/app.toml
+sed -i 's/"os"/"test"/' "$HOME"/.SmplChain/config/client.toml
+sed -i "s/SmplChain/$chainName/" "$HOME"/.SmplChain/config/client.toml
+jq ".chain_id = \"$chainName\"" "$genesisFile" >temp.json && mv temp.json "$genesisFile"
+jq '.chain_id = "cosmos:smpl-chain-testing"' "$genesisFile" >temp.json && mv temp.json "$genesisFile"
+jq '.app_state.mint.minter.inflation = "0"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+jq '.app_state.mint.params.mint_denom = "garden"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+jq '.app_state.mint.params.inflation_rate_change = "0"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+jq '.app_state.mint.params.inflation_max = "0"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+jq '.app_state.mint.params.inflation_min = "0"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+
+jq '.app_state.crisis.constant_fee.denom = "garden"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+jq '.app_state.staking.params.bond_denom = "garden"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+jq '.app_state.gov.deposit_params.min_deposit[0].denom = "garden"' "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+
+jq ".app_state.bank.denom_metadata = $denomMetadata" "$genesisFile" > temp.json && mv temp.json "$genesisFile"
+
+while read -r i; do
+  echo $i
+  j=$(sed 's/\s.*$//' <<<"$i")
+  r=$(sed 's/^\w* //' <<<"$i")
+  SmplChaind add-genesis-account "$j" "$r"
+done <$file
+
+cp -a keyring-test "$HOME"/.SmplChain/
+
+SmplChaind gentx validator2 100000000000garden
+
+SmplChaind collect-gentxs
